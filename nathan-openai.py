ca9334e00a1e36409dcc8d7c6f4d168020eaf80e
Chen Nuode <chennuode2012@gmail.com>
2024-11-09 15:16:40 -0500

    fixed up my own index.html

---
+++ nathan-openai.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+
+def example_chat(model_name: str, stream: bool = False):
+    """ 
+    Examples of chat completions from the proxy
+    """
+    client = OpenAI(
+        api_key="sk-t294DQLct5JulSvcXBgTAA", # set this!!!
+        base_url="https://nova-litellm-proxy.onrender.com" # and this!!!
+    )
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages = [
+            {
+                "role": "user",
+                "content": "Give me a api get request example?"
+            }
+        ],
+        stream=stream
+    )
+
+    #raw chunk
+    #for chunk in response:
+    #    print(chunk)
+    
+    print(response.choices[0].message.content) #message from the AI
+    #print(response[1][1])
+
+if __name__ == "__main__":
+    #example_chat("anthropic/claude-3-5-sonnet-20241022", stream=True)
+    #example_chat("anthropic/claude-3-5-sonnet-20241022", stream=False)
+    #example_chat("openai/gpt-4o", stream=True)
+    #example_chat("openai/gpt-4o", stream=False)
+    example_chat("gpt-4o-mini", stream=False)
+    #example_chat("gpt-4o-mini", stream=False)
+    #example_chat("gemini/gemini-1.5-pro", stream=True)
+    #example_chat("gemini/gemini-1.5-pro", stream=False)
