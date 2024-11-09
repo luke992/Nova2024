315738220a8aa0ccd79e613dcd0462031383fdfd
cheongalc <22002891+cheongalc@users.noreply.github.com>
2024-11-09 12:57:14 -0500

    minimal app ui

---
+++ atlas/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit_query', methods=['POST'])
+def submit_query():
+    data = request.get_json()
+    query = data.get('query', '')
+    print("Received query:", query)  # Print the natural language query to the console
+    return jsonify({"message": query})
+
+if __name__ == '__main__':
+    app.run(debug=True)
